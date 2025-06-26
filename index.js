b41eb12cd472ab27c64102c8006606f3e551b041
Donnie3004 <arbazrehman3004@gmail.com>
2025-06-27 01:14:43 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from 'express';
+import expressEjsLayouts from 'express-ejs-layouts';
+import path from 'path';
+
+
+import userController from './src/controllers/UserController.js';
+
+const app = express();
+const port = 8000;
+
+app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(path.resolve(), 'src', 'views'));
+app.use(expressEjsLayouts);
+app.set('layout', 'layout');
+
+const UserController = new userController();
+app.get("/", UserController.getLandingPage);
+
+app.listen(port,(err)=>{
+  if(err){
+    console.log("Error : " , err);
+  }
+  console.log(`Server Started at PORT : ${port}`);
+});
---
+++ src/controllers/userController.js
@@ -0,0 +1,5 @@
+export default class UserController {
+  getLandingPage(req, res){
+    res.render('landing-page');
+  }
+}
